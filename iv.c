e6c2ed9144f73fa6ca0f610edc1be64ec421c1aa
Shivsrijit Verma <shivsrijit@gmail.com>
2026-06-26 11:12:58 +0530

    setup done

---
+++ iv.c
@@ -0,0 +1,13 @@
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* win = SDL_CreateWindow(
+        "SDL2 Works!", SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+    SDL_Delay(6000);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+    return 0;
+}
